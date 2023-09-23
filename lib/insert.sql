272842f7890f1dd922a7b320476f8f4aa20dfcfe
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-23 18:28:15 -0400

    bears are in database

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "brown", "Medium", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "black", "high", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "golden", "low", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 9, "M", "brown", "Medium", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 13, "F", "brown", "low", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "black", "high", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "red", "low", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, "M", "brown", "Medium", 0);
+
